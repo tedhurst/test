2d66734aa486bfd0895cfc3e55cc3d0139666dca
tedhurst <hurstt17@mail.wlu.edu>
2016-04-26 12:45:10 -0400

    some message

---
+++ functional_tests.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+import unittest
+
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicity_wait(5)
+        
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Edith has heard about a cool new online to-do app. She goes
+        # to check out its homepage
+        self.browser.get('http://localhost:8000')
+
+        # she notices the page title and header mention to-do lists
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+        # She is invited to enter a to-do item staright away
+        # She types "Buy peacock feathers" into a text box
+        # When she hits enter, the page updates
+
+if __name__ == '__main__':
+    #v = NewVisitorTest()
+    #print(v.browser)
+    unittest.main()
+    print("done")
