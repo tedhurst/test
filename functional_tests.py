0170d19574e39f1902193af85c3ce2e3d5119eaf
tedhurst <hurstt17@mail.wlu.edu>
2016-04-27 10:34:45 -0400

    Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common import keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -15,13 +16,44 @@ class NewVisitorTest(unittest.TestCase):
         # to check out its homepage
         self.browser.get('http://localhost:8000')
 
-        # she notices the page title and header mention to-do lists
+        # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # She is invited to enter a to-do item staright away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+        
         # She types "Buy peacock feathers" into a text box
-        # When she hits enter, the page updates
+        inputbox.send_keys('Buy peacock feathers')
+        
+        # When she hits enter, the page updates and now lists the to-do list
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = self.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text=='1: Buy peacock feathers' for row in rows)
+        )
+
+        # There is still a text box inviting her to add another item. she
+        # enters "Use peacock feathers to make a fly"
+        self.fail('Finish the test!')
+
+        # The page updates again, and now shows both items on her list
+
+        # Edith wonders whether the site will remember her list. Then she sees
+        # that the site has generated a unique URL for her -- there is some
+        # explanatory text to that effect.
+
+        # She visits that URL - her to-do list is still there.
+
+        # Satisfied, she goes back to sleep.
+        
 
 if __name__ == '__main__':
     #v = NewVisitorTest()
