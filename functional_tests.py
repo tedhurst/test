dda4be0149e1f24be25f49d1fa6ce6769f37889a
tedhurst <hurstt17@mail.wlu.edu>
2016-04-27 12:33:14 -0400

    First page html now generated from template

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,5 @@
 from selenium import webdriver
-from selenium.webdriver.common import keys
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -9,6 +9,7 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.implicitly_wait(3)
         
     def tearDown(self):
+        #a=1
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
@@ -35,9 +36,10 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
 
         table = self.browser.find_element_by_id('id_list_table')
-        rows = self.find_elements_by_tag_name('tr')
+        rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text=='1: Buy peacock feathers' for row in rows)
+            any(row.text=='1: Buy peacock feathers' for row in rows),
+            "New to-do item did not appear in table"
         )
 
         # There is still a text box inviting her to add another item. she
