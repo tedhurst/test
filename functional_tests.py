3c571f7ec21bd752a9ba2d4f991ee8bee770a22f
tedhurst <hurstt17@mail.wlu.edu>
2016-04-28 13:40:47 -0400

    redirect after POST, and show all items in template

--- functional_tests.py
+++ functional_tests.py
@@ -40,17 +40,18 @@ class NewVisitorTest(unittest.TestCase):
         
         # When she hits enter, the page updates and now lists the to-do list
         inputbox.send_keys(Keys.ENTER)
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         #time.sleep(7)
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
+        #table = self.browser.find_element_by_id('id_list_table')
+        #rows = table.find_elements_by_tag_name('tr')
 ##        self.assertTrue(
 ##            any(row.text=='1: Buy peacock feathers' for row in rows),
 ##            "New to-do item did not appear in table -- its text was:\n%s" %(
 ##                table.text,
 ##            )
 ##        )
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        #self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 
         # There is still a text box inviting her to add another item. she
         # enters "Use peacock feathers to make a fly"
@@ -59,13 +60,15 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
 
         # The page updates again, and now shows both items on her list
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn(
-            '2: Use peacock feathers to make a fly',
-            [row.text for row in rows]
-        )
+##        table = self.browser.find_element_by_id('id_list_table')
+##        rows = table.find_elements_by_tag_name('tr')
+##        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+##        self.assertIn(
+##            '2: Use peacock feathers to make a fly',
+##            [row.text for row in rows]
+##        )
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
         
 
         # Edith wonders whether the site will remember her list. Then she sees
