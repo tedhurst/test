b295cc975342de509e86a92b7a460de09aa13809
tedhurst <hurstt17@mail.wlu.edu>
2016-04-28 22:20:45 -0400

    new URL, view and template to display lists

--- lists/views.py
+++ lists/views.py
@@ -14,9 +14,9 @@ def home_page(request):
 ##        new_item_text = request.POST['item_text']
 ##        Item.objects.create(text=new_item_text)
         Item.objects.create(text=request.POST['item_text'])
-        return redirect('/')
+        return redirect('/lists/the-only-list-in-the-world/')
 
-    items = Item.objects.all()
+    #items = Item.objects.all()
     return render(request, 'home.html', {'items': items})
 ##    else:
 ##        new_item_text = ''
@@ -31,3 +31,7 @@ def home_page(request):
 ##    return render(request, 'home.html')
 
 #home_page = None
+
+def view_list(request):
+    items = Item.objects.all()
+    return render(request, 'list.html', {'items': items}) 
